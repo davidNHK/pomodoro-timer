e60245e6790e405d093255d2ee685e9b11b1ba5c
David Ng <david.ng.dev@gmail.com>
2022-04-21 23:05:37 +0100

    chore: re-install package-lock.json

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-npx lint-staged
+npx lint-staged --allow-empty
 npx eslint --ext .json,.yaml,.yml,.ts,.js --ignore-pattern '!.github/' --ignore-pattern systems/ --ignore-pattern package-lock.json .
